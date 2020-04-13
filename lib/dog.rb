8363edbfaee99e50cf7d4212b11e26afcbc9e901
Sam Bequette <sbb3391@gmail.com>
2020-04-13 19:32:29 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,12 +4,14 @@ class Dog
     @name = name
   end
 
+  def name=(new_name)
+    @name = new_name
+  end
+  
   def name
     @name
   end
 
-  def name=(new_name)
-    @name = new_name
-  end
+
 
 end
